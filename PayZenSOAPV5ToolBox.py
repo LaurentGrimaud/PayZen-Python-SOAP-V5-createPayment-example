cf0e668b1982d7652f5ec87d6ef064fc93226050
Laurent Grimaud <laurent.grimaud@gmail.com>
2015-12-08 17:20:21 +0100

    Add logging capability

--- PayZenSOAPV5ToolBox.py
+++ PayZenSOAPV5ToolBox.py
@@ -1,3 +1,14 @@
+##
+ # PayZen SOAP V5 Tool Box
+ # Uses SUDS as SOAP backend
+ #
+ # @depend SUDS
+ # @link https://fedorahosted.org/suds/
+ #
+ # @version 0.2
+ # 
+ ##
+
 from suds.client import Client
 from suds.sax.element import Element
 
@@ -7,19 +18,30 @@ import hmac
 import base64
 from datetime import datetime
 from hashlib import sha256 
+import logging
 
 
 class PayZenSOAPV5ToolBox:
   # PayZen platform data
   platform = {
-            'wsdl'           : 'https://secure.payzen.eu/vads-ws/v5?wsdl',
-	    'ns'             : 'http://v5.ws.vads.lyra.com/',
-	    'hns'            : 'http://v5.ws.vads.lyra.com/Header',
+            'wsdl'           : 'https://secure.payzen.eu/vads-ws/v5?wsdl', # URL of the PayZen SOAP V5 WSDL
+	    'ns'             : 'http://v5.ws.vads.lyra.com/',              # Namespace of the service
+	    'hns'            : 'http://v5.ws.vads.lyra.com/Header',        # Namespace ot the service header
   }
+
+  # UUID V5 needs a valid UUID as reference
   UUIDBase = '1546058f-5a25-4334-85ae-e68f2a44bbaf'
 
-  # PayZen account data
-  def __init__(self, shopId, certTest, certProd, mode = 'TEST'):
+  # Constructor, stores the PayZen user's account informations
+  # 
+  # @param shopId string, the account shop id as provided by Payzen
+  # @param certTest string, certificate, test-version, as provided by PayZen
+  # @param certProd string, certificate, production-version, as provided by PayZen
+  # @param mode string ("TEST" or "PRODUCTION"), the PayZen mode to operate
+  # @param logger logging.logger object, the logger to use. Will be created if not provided
+  #
+  def __init__(self, shopId, certTest, certProd, mode = 'TEST', logger = None):
+    self.logger = logger or logging.getLogger()
     self.account = {
      'shopId': shopId,
      'cert': {
@@ -29,7 +51,12 @@ class PayZenSOAPV5ToolBox:
      'mode': mode
     } 
 
-  #Header construct
+  # Utility method, build the SOAP headers
+  #
+  # @param timestamp string, the SOAP header timestamp
+  #
+  # @return dict of SUDS.Element defining the headers
+  #
   def headers(self, timestamp):
    # Mandatory header data
    requestId = str(uuid.uuid5(uuid.UUID(self.UUIDBase), timestamp))
@@ -45,13 +72,28 @@ class PayZenSOAPV5ToolBox:
      )
    return headers
 
-
+  # Utility method, builds the authToken matching the
+  # given requestId and timestamp
+  #
+  # @param requestId string, the request UUID
+  # @param timestamp string, the request timestamp
+  # @param format string, the format to use: 'request'
+  # or 'response'
+  #
+  # @return dict of SUDS.SAX.Element defining the headers
+  #
   def authToken(self, requestId, timestamp, format = 'request'):
     certificate = self.account['cert'][self.account['mode']]
     data = str(requestId) + timestamp if format == 'request' else timestamp + str(requestId)
     return base64.b64encode(hmac.new(certificate, data, sha256).digest())
 
 
+  # Utility method, validates the response from PayZen
+  #
+  # @param answer SUDS answer
+  #
+  # @throw Exception if response is invalid
+  #
   def validate(self, answer):
     try:
      headers = answer.getChild("soap:Envelope").getChild("soap:Header")
@@ -59,32 +101,48 @@ class PayZenSOAPV5ToolBox:
      timestamp = headers.getChild('timestamp').getText()
      authToken = headers.getChild('authToken').getText()
     except:
-     raise Exception('Incorrect SOPA header in response - Payment is not confirmed')
+     raise Exception('Incorrect SOAP header in response - Payment is not confirmed')
     if authToken != self.authToken(requestId, timestamp, 'response'):
       raise Exception('Received authToken incorrect - Payment is not confirmed')
+    self.logger.debug('auth token {} for request id {} is valid'.format(authToken, requestId))
 
 
+  # Main method, performs a createRequest payment
+  #
+  # @param amount string, the payment amount
+  # @param currency string, the currency code (978 is for Euro)
+  # @param cardNumber string, the credit card number
+  # @param expiryMonth string, the month (MM) of the credit card expiry
+  # @param expiryMonth string, the year (YYYY) of the credit card expiry
+  # @param cardSecurityCode string, the security code of the credit card 
+  # @param cardSecurityCode string, the scheme of the credit card (ie 'VISA')
+  # @param orderId string, the identifier of the order related to the requested payment
+  #
+  # @return SUDS answer
+  #
   def createPayment(self, amount, currency, cardNumber, expiryMonth, expiryYear, cardSecurityCode, scheme, orderId):
-  # Create a service proxy from the WSDL.
+   self.logger.info("'createPayment' requested for order id {} (amount: {}, currency: {})".format(orderId, amount, currency))
+   # Create a SUDS client of the PayZen platform
    client = Client(url = self.platform['wsdl'])
- #  print client
 
+   # Each request needs a timestamp
    timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
+   # SOAP headers construction and definition
    headers = self.headers(timestamp)
    client.set_options(soapheaders = headers)
 
-   # Build the payload
-   ## commonRequest
+   # Builds the payload
+   ## commonRequest part
    commonRequest = {'submissionDate': timestamp}
 
-   ## paymentRequest
+   ## paymentRequest part
    paymentRequest = {'amount': amount, 'currency': currency}
 
-   ## orderRequest
+   ## orderRequest part
    orderRequest = {'orderId': orderId}
 
-   ## cardRequest
+   ## cardRequest part
    cardRequest = {
        'number'           : cardNumber
      , 'expiryMonth'      : expiryMonth 
@@ -93,7 +151,7 @@ class PayZenSOAPV5ToolBox:
      , 'scheme'           : scheme
    }
 
-   # Perform the query.
+   # Performs the query
    answer = client.service.createPayment(
      commonRequest  = commonRequest,
      paymentRequest = paymentRequest,
@@ -101,8 +159,9 @@ class PayZenSOAPV5ToolBox:
      orderRequest   = orderRequest
      )
 
-   # Validate the answer
+   # Validates the answer
    self.validate(client.last_received())
+   self.logger.info("'createPayment' response for order id {} is valid".format(orderId))
 
-   #returns the answer
+   # Returns the answer
    return answer
--- payzen.soap-v5.createPayment.example.py
+++ payzen.soap-v5.createPayment.example.py
@@ -1,37 +1,61 @@
+ ##
+ # PayZen SOAP V5 payment example
+ #
+ # @version 0.2
+ # 
+ ####
 from PayZenSOAPV5ToolBox import PayZenSOAPV5ToolBox
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+# Here we define the logger we wanna use
+# Adapt this to reflect your needs
+logging.basicConfig(filename="payment_soap.log", level=logging.INFO)
+logger = logging.getLogger()
 
-#Payment data
-amount           = 1000
-currency         = 978
-cardNumber       = '4970100000000003'
-expiryMonth      = '11'
-expiryYear       = '2016'
-cardSecurityCode = '235'
-scheme           = 'VISA'
+# Payment data
+amount           = 1000                # payment amount, in cents
+currency         = 978                 # 'EURO' currency code
+cardNumber       = '4970100000000003'  # debit/credit card number
+expiryMonth      = '11'                # debit/credit card month expiry (2 digits)
+expiryYear       = '2016'              # debit/credit card year expiry (4 digits)
+cardSecurityCode = '235'               # debit/credit card security code
+scheme           = 'VISA'              # debit/credit card scheme
 
-#Order data
-orderId  = '[***CHANGE-ME***]'
+# Order data
+orderId          = '12345678'          # 'your' order id
 
-#Account data
-shopId   = '[***CHANGE-ME***]'
-certTest = '[***CHANGE-ME***]'
-certProd = '[***CHANGE-ME***]'
-mode     = 'TEST'
+# Account data
+shopId   = '[***CHANGE-ME***]'         # PayZen shop id
+certTest = '[***CHANGE-ME***]'         # PayZen certificate for TEST mode
+certProd = '[***CHANGE-ME***]'         # PayZen certificate for PRODUCTION mode
+mode     = 'TEST'                      # the mode you wanna the payment to be in
 
-payzen = PayZenSOAPV5ToolBox(shopId, certTest, certProd, mode)
 
-result = payzen.createPayment(amount, currency, cardNumber, expiryMonth, expiryYear, cardSecurityCode, scheme, orderId)
-   
+# Initialisation of the PayZen Tool-Box
+payzenTB = PayZenSOAPV5ToolBox(shopId, certTest, certProd, mode, logger)
 
-# Output the result.
-if result.commonResponse.responseCode == 0:
-  print 'Payment is done !'
-else:
-  print "Something was wrong during payment"
-  print "PayZen responded with the code " + result.commonResponse.responseCode
-  if result.commonResponse.responseCodeDetail:
-    print "The additionnal given detail was: " + result.commonResponse.responseCodeDetail
+# 'createPayent' request
+try:
+  result = payzenTB.createPayment(amount, currency, cardNumber, expiryMonth, expiryYear, cardSecurityCode, scheme, orderId)
+  logger.info("Payzen response code was: " + str(result.commonResponse.responseCode))
+
+  # Use the result
+  if result.commonResponse.responseCode == 0:
+    ### Here is the code for an accepted payment
+    print 'Payment is done !'
+    logger.info('Payment of order {} is done !'.format(orderId))
+  else:
+    ### Here is the code for a not accepted payment
+    print "Payment is not authorised"
+    logger.error("Payment not authorised for order " + orderId)
+    print "PayZen responded with code " + str(result.commonResponse.responseCode)
+    if result.commonResponse.responseCodeDetail:
+      print "The additionnal given detail was: " + result.commonResponse.responseCodeDetail
+      logger.info("The additionnal given detail was: " + result.commonResponse.responseCodeDetail)
+
+except Exception as e:
+  ### Here is the code for a error during process management
+  msg = 'Something was wrong during payment process - An exception raised: ' + str(e)
+  logger.error(msg)
+  print msg
